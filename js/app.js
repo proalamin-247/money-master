5116bc43fe695ca7a2c3141389b4d58bf848eabe
proalamin-247 <pro.alamin247@gmail.com>
2022-02-17 05:06:43 +0600

    total expenses & balance & save calculation done

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ function inputAmount(incomeValue){
     const getAmountInput = document.getElementById(incomeValue);
     const newAmountInput = getAmountInput.value;
     const amaountInput = parseFloat(newAmountInput);
+    
+    // clear
+    // getAmountInput.value = ''; ইনপুট এর ভ্যালু গুলা ক্লিয়ার করবো নাকি করবো না ??
     return amaountInput;
 }
 
@@ -14,6 +17,19 @@ function inputAmount(incomeValue){
     return totaldefaultBalance;
 }
  */
+
+// total expemses amount
+function totalExpensesAmount(){
+    const foodCost = inputAmount('food-cost');
+    const rentCost = inputAmount('rent-cost');
+    const clothCost = inputAmount('cloth-cost');
+    const totalExpensesAll = totaldefaultBalance + foodCost + rentCost + clothCost;
+    return totalExpensesAll;
+}
+
+const defaultBalanceFiled = document.getElementById('total-expenses');
+const defaultBalanceText = defaultBalanceFiled.innerText;
+const totaldefaultBalance = parseFloat(defaultBalanceText);
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
 
@@ -40,19 +56,58 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     // const mainBalance = defaultBalance('main-balance');
     // myIncomeAmount.value = mainBalance.innerText;
 
-    const defaultBalanceFiled = document.getElementById('total-expenses');
-    const defaultBalanceText = defaultBalanceFiled.innerText;
-    const totaldefaultBalance = parseFloat(defaultBalanceText);
+    // const defaultBalanceFiled = document.getElementById('total-expenses');
+    // const defaultBalanceText = defaultBalanceFiled.innerText;
+    // const totaldefaultBalance = parseFloat(defaultBalanceText);
     
-
-    const totalExpenses = totaldefaultBalance + foodCost + rentCost + clothCost;
-    defaultBalanceFiled.innerText = totalExpenses;
-
     const balanceAmountField = document.getElementById('main-balance');
     const balanceAmountText = balanceAmountField.innerText;
     const balanceAmountRemain = parseFloat(balanceAmountText);
 
+    // const totalExpenses = totaldefaultBalance + foodCost + rentCost + clothCost;
+    const totalExpenses = totalExpensesAmount();
+    // defaultBalanceFiled.innerText = totalExpenses;
+
     const newBalanceRemain = myIncomeAmount - totalExpenses;
-    balanceAmountField.innerText = newBalanceRemain;
+    // balanceAmountField.innerText = newBalanceRemain;
+
+    if(totalExpenses > 0 && newBalanceRemain > 0){
+        defaultBalanceFiled.innerText = totalExpenses;
+        balanceAmountField.innerText = newBalanceRemain;
+    }
+    else if(totalExpenses > myIncomeAmount ){
+        alert('much more expenses')
+    }
+    else {
+        alert("Write possitive value! & number type (English type)*");
+    }
+
+})
+
+// save 
+document.getElementById('saving-btn').addEventListener('click', function(){
+    
+    const savingAmountParsent = inputAmount('saving-amount-parsent');
+
+    const myIncomeAmount = inputAmount('income-input');
+
+    const savingAmountField = document.getElementById('saving-amount');
+    const savingAmountText = savingAmountField.innerText;
+    const savingAmount = parseFloat(savingAmountText);
+
+    const newSavingAmount = myIncomeAmount / savingAmountParsent;
+    savingAmountField.innerText = newSavingAmount;
+
+    const remainingBalanceField = document.getElementById('remaining-balance');
+    const remainingBalanceText = remainingBalanceField.innerText;
+    const remainingBalance = parseFloat(remainingBalanceText);
+    
+    const balanceAmountField = document.getElementById('main-balance');
+    const balanceAmountText = balanceAmountField.innerText;
+    const balanceAmountRemain = parseFloat(balanceAmountText);
+
+    const newBalanceRemain = balanceAmountRemain - newSavingAmount;
+    remainingBalanceField.innerText = newBalanceRemain;
+
 
 })
